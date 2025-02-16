8cf3e76687eb968d2b481f591a0d39a8e9b125cb
Vladimir <voltvv@yandex.ru>
2025-02-16 17:15:29 +0300

    Car and Race classes added code to Main class added

---
+++ src/main/java/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String carName;
+    int carSpeed;
+    public Car(String carName, int carSpeed) {
+        this.carName = carName;
+        this.carSpeed = carSpeed;
+    }
+}
